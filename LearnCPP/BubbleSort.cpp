6ab492f4c849912dfa63ceb014293d4fb8ba1461
Sumeet Agrawal <sumeetagrawal126@gmail.com>
2021-06-07 12:02:11 +0530

    Updated boundary conditions

--- LearnCPP/BubbleSort.cpp
+++ LearnCPP/BubbleSort.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <utility>
 
@@ -6,14 +5,21 @@ int main()
 {
 	int array[]{ 6, 3, 2, 9, 7, 1, 5, 4, 8 };
 	constexpr int length{ static_cast<int>(std::size(array)) };
-
-	for (int i = 0; i < length; i++)
+	bool b_isSorted = false;
+	for (int i = 0; i < length - 1; i++)
 	{
+		if (b_isSorted)
+		{
+			std::cout << "Early termination on iteration " << i << std::endl;
+			break;
+		}
+		b_isSorted = true;
 		for (int j = 0; j < length - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				std::swap(array[j], array[j + 1]);
+				b_isSorted = false;
 			}
 		}
 	}
