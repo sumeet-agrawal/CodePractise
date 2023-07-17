ba01bdecc0187b870a868213f43fe6b03e0f5beb
Sumeet Agrawal <sumeetagrawal126@gmail.com>
2023-07-17 11:17:03 +0530

    Learn CPP final

---
+++ LearnCPP/ptr/ptr.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <memory> // for std::shared_ptr
+
+class Resource
+{
+public:
+	Resource() { std::cout << "Resource acquired\n"; }
+	~Resource() { std::cout << "Resource destroyed\n"; }
+};
+
+int main()
+{
+	std::shared_ptr<Resource> ptr1{ std::make_shared<Resource>() };
+	std::shared_ptr<Resource> ptr2{ ptr1 };
+
+	return 0;
+}
