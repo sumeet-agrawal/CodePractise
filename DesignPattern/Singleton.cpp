6715f8a7eabdd50a1c2593f663603a79ee2f3d19
Sumeet Agrawal <sumeetagrawal126@gmail.com>
2023-09-07 13:56:22 +0530

    Singleton

--- DesignPattern/Singleton.cpp
+++ DesignPattern/Singleton.cpp
@@ -1,15 +1,43 @@
-class Core
-{
+#include <iostream>
+
+template<typename T>
+class Singleton {
 public:
-    static Core& instance()
+    static T& instance()
     {
-        static Core t;
-        return t;
+        static T instance{};
+        return instance;
     }
-    Core& operator=(const Core&) = delete;
-    Core(const Core&) = delete;
-    Core(Core&&) = delete;
-    Core& operator=(Core&&) = delete;
-private:
-    Core() = default;
+    Singleton(const Singleton&) = delete;
+    Singleton& operator= (const Singleton) = delete;
+    Singleton(Singleton&&) = delete;
+    Singleton& operator=(Singleton&&) = delete;
+
+protected:
+    Singleton() {}
 };
+
+
+class Test final : public Singleton<Test>
+{
+    friend class Singleton<Test>;
+private:
+    Test() { std::cout << "constructed\n"; }
+    ~Test() {  std::cout << "destructed\n"; }
+public:
+    void use() const { std::cout << "in use" << std::endl; };
+};
+
+int main()
+{
+    std::cout << "Entering main()" << std::endl;
+    {
+        auto const& t = Test::instance();
+        t.use();
+    }
+    {
+        auto const& t = Test::instance();
+        t.use();
+    }
+    std::cout << "Leaving main()" << std::endl;
+}
