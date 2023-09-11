99401bde79f4bbdfe03cef30b70e971127fab4fc
Sumeet Agrawal <sumeetagrawal126@gmail.com>
2023-09-11 15:25:50 +0530

    Update

--- DataStructure/def.cpp
+++ DataStructure/def.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 
+using namespace std;
+
+class A
+{
+protected:
+    string name;
+public:
+    A(string_view sv)
+        : name{sv}
+    {}
+};
+class B: public A
+{
+public:
+    B(string_view sv)
+        : A(sv)
+    {}
+    void print()
+    {
+        cout<<name<<"\n";
+    }
+};
+
 int main()
 {
+    B b("Sumeet");
+    b.print();
     return 0;
 }
