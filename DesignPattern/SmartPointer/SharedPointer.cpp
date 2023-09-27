f6a5d373bac89c640918ca4ef2bf5661d960b346
Sumeet Agrawal <sumeetagrawal126@gmail.com>
2023-09-27 12:02:27 +0530

    Practise

--- DesignPattern/SmartPointer/SharedPointer.cpp
+++ DesignPattern/SmartPointer/SharedPointer.cpp
@@ -1,88 +1,127 @@
 #include <iostream>
 
+using namespace std;
+
 class Counter
 {
 private:
-    unsigned int m_counter{};
+    int cnt{};
 public:
-    Counter()
-        : m_counter{}
-    {}
-    ~Counter()  {}
+    Counter() = default;
+    ~Counter() = default;
+
     Counter(const Counter&) = delete;
     Counter& operator=(const Counter&) = delete;
-    void operator++(){m_counter++;}
-    void operator++(int){m_counter++;}
-    void operator--(){m_counter--;}
-    void operator--(int){m_counter--;}
-    int get() { return m_counter;}
+    Counter(Counter&&) = delete;
+    Counter& operator=(Counter&&) = delete;
+
+    int getCount() const { return cnt;}
+    Counter& operator++() { cnt++; return *this;}
+    Counter& operator--() { cnt--; return *this;}
 };
 
-template<typename T>
+template <typename T>
 class SharedPtr
 {
 private:
-    T* t{};
-    Counter* m_counter{};
+    Counter* cntr{};
+    T* ptr{};
 public:
-    explicit SharedPtr(T* obj = nullptr)
-        : t{obj}
+    explicit SharedPtr(T* res)
+        : ptr{res}
+        , cntr{new Counter()}
     {
-        m_counter = new Counter();
-        (*m_counter)++;
-    }
-    SharedPtr(SharedPtr& ptr)
-    {
-        t = ptr.t;
-        m_counter = ptr.m_counter;
-        (*m_counter)++;
+        ++(*cntr);
     }
     ~SharedPtr()
     {
-        (*m_counter)--;
-        if(!m_counter->get())
-        {
-            delete t;
-            delete m_counter;
-        }
+        --(*cntr);
+        if(cntr->getCount())
+            return;
+        delete ptr;
+        delete cntr;
     }
+    SharedPtr(const SharedPtr& sptr)
+        : ptr{sptr.ptr}
+        , cntr{sptr.cntr}
+    {
+        ++(*cntr);
+    }
+    SharedPtr& operator=(const SharedPtr& sptr)
+    {
+        if(ptr == sptr.ptr)
+            return *this;
+        ptr = sptr.ptr;
+        cntr = sptr.cntr;
+        ++(*cntr);
+        return *this;
+    }
+    T* operator->() { return ptr;}
+    T& operator*() { return *ptr;}
+    void printCount() { cout<<cntr->getCount()<<"\n";}
+    T* get() const { return ptr;}
 };
 
 template <typename T>
 class WeakPtr
 {
-    T* t{};
+private:
+    T* ptr{};
 public:
-    WeakPtr(SharedPtr& ptr)
+    WeakPtr(const SharedPtr<T>& sptr)
+        : ptr{sptr.get()}
+    {}
+    WeakPtr& operator=(const SharedPtr<T>& sptr)
     {
-        t = ptr.t;
-    }
-    WeakPtr& operator=(SharedPtr& ptr)
-    {
-        t = ptr.t;
+        if(ptr == sptr.get())
+            return *this;
+        ptr = sptr.get();
         return *this;
     }
-    ~WeakPtr() = default;
+    WeakPtr(const WeakPtr& wptr)
+        : ptr{wptr.ptr}
+    {}
+    WeakPtr& operator=(const WeakPtr& wptr)
+    {
+        if(ptr == wptr.ptr)
+            return *this;
+        ptr = wptr.ptr;
+        return *this;
+    }
+    T* operator->() { return ptr;}
+    T& operator*() { return *ptr;}
 };
 
 class Resource
 {
+private:
+    static int getP() 
+    { 
+        static int n{};
+        return ++n;
+    }
 public:
-	Resource() { std::cout << "Resource acquired\n"; }
-	~Resource() { std::cout << "Resource destroyed\n"; }
+    void print()    { cout<<"Hello World "<<getP()<<"\n";}
+    Resource() { cout<<"Resource created\n";}
+    ~Resource(){ cout<<"Resource destroyed\n";}
 };
 
 int main()
 {
-	Resource* res { new Resource };
-	SharedPtr<Resource> ptr1 { res };
-	{
-		SharedPtr<Resource> ptr2 { res }; // create ptr2 directly from res (instead of ptr1)
-
-		std::cout << "Killing one shared pointer\n";
-	} // ptr2 goes out of scope here, and the allocated Resource is destroyed
-
-	std::cout << "Killing another shared pointer\n";
-
-	return 0;
-} // ptr1 goes out of scope here, and the allocated Resource is destroyed again
+    SharedPtr<Resource> sptr(new Resource());
+    {
+        WeakPtr<Resource> wptr{sptr};
+        wptr->print();
+        sptr.printCount();
+        wptr->print();
+    }
+    cout<<"Here\n";
+    {
+        SharedPtr<Resource> nptr{sptr};
+        nptr.printCount();
+    }
+    sptr->print();
+    sptr.printCount();
+    (*sptr).print();
+    return 0;
+}
